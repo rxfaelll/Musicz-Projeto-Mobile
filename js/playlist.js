8ba6d9b848280e4c4bbf63609fb300c17d7122c9
Rxfaelll <rafa.081299@gmail.com>
2019-09-15 22:46:14 -0300

    Criando arquivo separado para o objeto das músicas

---
+++ js/playlist.js
@@ -0,0 +1,23 @@
+//Cria objetos HTML com as informação dos objetos JSON. mas como isso, Rafael? Calma lá, minha Beldade
+for(let i=0; i<songsObj.length; i++){
+    let musicBtn = document.createElement("div"); //Primeiro a gente cria a "gaiola" que vai representar o botão
+    musicBtn.classList.add("music-btn"); //Depois, coloca uma classe nele, pra estilizar no css
+    document.getElementById("containerPlaylist").appendChild(musicBtn); //Agora a gente seleciona o container que vai guardar os botões e coloca nosso botão dentro dele
+
+    let songName = document.createElement("h2");//Agora é criar o H2 que vai ser o nome da música
+    songName.innerHTML = songsObj[i]["name"];//Fazer o nosso H2 receber a informação do Objeto JSON
+    musicBtn.appendChild(songName);//E pimba, colocar dele dentro da gaiolinha que foi criada lá em cima
+
+    let authorName = document.createElement("h3");//E aqui é só repetir o processo pro H3 do artista
+    authorName.innerHTML = songsObj[i]["author"];
+    musicBtn.appendChild(authorName);
+
+    let audioTag = document.createElement("audio");
+    audioTag.setAttribute("id", "musica"+i)
+    musicBtn.appendChild(audioTag)
+
+    let audioUrl = document.createElement("source");
+    audioUrl.setAttribute("src", songsObj[i]["url"])
+    audioTag.appendChild(audioUrl);
+}
+
---
+++ js/songsObj.js
@@ -0,0 +1,58 @@
+var songsObj = [
+
+    {
+        "name": "Cold Cold Man",
+        "author": "Saint Motel",
+        "TimeInSeconds": 231,
+
+        "url": "audio/ColdColdMan.mp3"
+    },
+
+    {
+        "name": "Wild Roses",
+        "author": "Of Monsters and Men",
+        "TimeInSeconds": 243,
+
+        "url": "audio/WildRoses.mp3"
+    },
+
+    {
+        "name": "Not My Baby",
+        "author": "Alvvays",
+        "TimeInSeconds": 256,
+
+        "url": "audio/NotMyBaby.mp3"
+    },
+
+    {
+        "name": "I'll die anyway",
+        "author": "girl in red",
+        "TimeInSeconds": 193,
+
+        "url": "audio/IllDieAnyway.mp3"
+    },
+
+    {
+        "name": "Ready To Let Go",
+        "author": "Cage The Elephant",
+        "TimeInSeconds": 187,
+
+        "url": "audio/ReadyToLetGo.mp3"
+    },
+
+    {
+        "name": "Social Cues",
+        "author": "Cage The Elephant",
+        "TimeInSeconds": 219,
+
+        "url": "audio/SocialCues.mp3"
+    },
+
+    {
+        "name": "Electric Love",
+        "author": "BØRNS",
+        "TimeInSeconds": 218,
+
+        "url": "audio/ElectricLove.mp3"
+    }
+]
